70af1a68ac6c92ee480414786ee11d1272599f6b
neco222 <107422195+neco222@users.noreply.github.com>
2025-11-26 06:30:29 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
 chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
-    // DeepL 翻訳
+
     if (req.type === 'TRANSLATE') {
         const { text, apiKey } = req.payload;
         const endpoint = apiKey.endsWith(':fx') 
-            ? 'https://api-free.deepl.com/v2/translate' 
-            : 'https://api.deepl.com/v2/translate';
+            ? 'https:
+            : 'https:
 
         fetch(endpoint, {
             method: 'POST',
@@ -38,7 +38,7 @@ chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
 
             body: JSON.stringify({ track, artist, youtube_url })
         })
-        .then(r => r.text())
+        .then(r => r.text()) 
         .then(text => {
             let lyrics = '';
             try {
@@ -76,6 +76,5 @@ chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
 
         return true;
     }
-
 
 });
